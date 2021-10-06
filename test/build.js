db0cf225aab1b93c58d5c6776fa1aa7452d9b6da
linux_china <libing.chen@gmail.com>
2021-10-05 19:16:46 -0700

    Add write support

--- test/build.js
+++ test/build.js
@@ -6,7 +6,8 @@ esbuild.build({
     bundle: true,
     entryPoints: ['hello.js'],
     plugins: [brotliPlugin],
-    write: false
+    write: true,
+    outfile: "bundle.js"
 }).then(result => {
     console.log(result.outputFiles[0].text);
 }).catch(() => process.exit(1));
