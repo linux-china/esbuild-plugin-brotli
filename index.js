9e63273ba2c68691d97daf6df99f50ce82988ec0
linux_china <libing.chen@gmail.com>
2021-10-05 19:31:05 -0700

    add upath to fix windows path problem

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const {compress} = require('brotli-wasm');
 const fs = require('fs');
+const {resolve} = require('path');
+const upath = require('upath');
 const FileHound = require('filehound');
 
 const name = 'brotli'
@@ -15,7 +17,7 @@ const setup = ({onResolve, onLoad}) => {
 
 const brotliCompress = async ({path, pluginData}) => {
     const resolveDir = pluginData.resolveDir;
-    const originalPath = resolveDir + "/" + path.replace("?br", "").replace("./", "");
+    const originalPath = resolve(resolveDir, path.replace("?br", ""));
     let stats = fs.lstatSync(originalPath);
     if (stats.isFile()) {// compress one file
         const rawBuffer = await fs.promises.readFile(originalPath);
@@ -28,9 +30,10 @@ const brotliCompress = async ({path, pluginData}) => {
             .ignoreHiddenFiles()
             .ignoreHiddenDirectories()
             .find();
-        const offset = originalPath.endsWith("/") ? originalPath.length - 1 : originalPath.length
+        const originalPathUnix = upath.toUnix(originalPath);
+        const offset = originalPathUnix.endsWith("/") ? originalPathUnix.length - 1 : originalPathUnix.length
         for (const file of files) {
-            const relativeFilePath = file.substring(offset);
+            const relativeFilePath = upath.toUnix(file).substring(offset);
             const rawBuffer = await fs.promises.readFile(file);
             const compressedBytes = compress(new Uint8Array(rawBuffer));
             const base64Text = Buffer.from(compressedBytes).toString("base64");
@@ -38,6 +41,8 @@ const brotliCompress = async ({path, pluginData}) => {
         }
         const binaryFunction = `const __toBinary2=(()=>{for(var r=new Uint8Array(128),a=0;a<64;a++)r[a<26?a+65:a<52?a+71:a<62?a-4:4*a-205]=a;return a=>{for(var t=a.length,e=new Uint8Array(3*(t-("="==a[t-1])-("="==a[t-2]))/4|0),n=0,o=0;n<t;){var A=r[a.charCodeAt(n++)],h=r[a.charCodeAt(n++)],c=r[a.charCodeAt(n++)],d=r[a.charCodeAt(n++)];e[o++]=A<<2|h>>4,e[o++]=h<<4|c>>2,e[o++]=c<<6|d}return e}})();`;
         return {contents: binaryFunction + ` export default { ${compressedFiles.join(",")} } `};
+    } else {
+        throw  new Error("brotli plugin: please import file or path!");
     }
 }
 
